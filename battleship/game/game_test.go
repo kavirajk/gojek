6f3cb5a2cb8a70383d0d767cb29f16720ebcfc83
Kaviraj <kavirajkanagaraj@gmail.com>
2017-02-25 01:10:58 +0530

    Basic test for simulation logic

--- battleship/game/game_test.go
+++ battleship/game/game_test.go
@@ -54,3 +54,49 @@ func TestNewGame(t *testing.T) {
 
 	}
 }
+
+// Test the play() logic, by testing p1 and p2 score for different inputs
+func TestGameResult(t *testing.T) {
+	var tests = []struct {
+		input            io.Reader
+		P1Score, P2Score int
+	}{
+		{
+			input: strings.NewReader(`5
+5
+1:1,2:0,2:3,3:4,4:3
+0:1,2:3,3:0,3:4,4:1
+5
+0,1:4,3:2,3:3,1:4,1
+0,1:0,0:1,2:2,3:4,3
+`),
+			P1Score: 3,
+			P2Score: 2,
+		},
+		{
+			input: strings.NewReader(`5
+5
+1:1,2:0,2:3,3:4,4:3
+0:1,2:3,3:0,3:4,4:1
+5
+0,1:4,3:2,3:3,1:4,1
+0,1:0,0:1,1:2,3:4,3
+`),
+			P1Score: 3,
+			P2Score: 3,
+		},
+	}
+
+	for _, test := range tests {
+		output := New(test.input)
+		output.Play() // Simulate Play
+		if output.P1Score() != test.P1Score {
+			t.Errorf("failed. P1Score incorrect. exptected %d, got %d", test.P1Score, output.P1Score())
+		}
+		if output.P2Score() != test.P2Score {
+			t.Errorf("failed. P2Score incorrect. exptected %d, got %d", test.P2Score, output.P2Score())
+		}
+
+	}
+
+}
